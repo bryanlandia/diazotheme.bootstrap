aeeecc4165ef501284f7f9a6b818ad7ac737d69a
Bryan Wilson <bryanlandia@gmail.com>
2014-09-03 14:27:14 -0700

    Use latest Bootstrap3 instead of 2 with custom breakpoints and grid sizes. Add the fonts, base css, base js. Use JQuery 1.9.1 not 1.6.2 Use Response.js and set the breakpoints on the body tag. Create basic layout HTML structure Make navigation bar and header HTML. Basics of phone-sized responsive navigation. Hide/show site search field using Bootstrap Collapse plugin. Add some Yes! images.

--- yes/diazotheme/diazo_resources/js/script.js
+++ yes/diazotheme/diazo_resources/js/script.js
@@ -8,14 +8,33 @@ $(document).ready(function () {
             $(el).remove();
         }
     });
-    jQuery17('.dropdown-toggle').click(function () {
-        var self = $(this).parent();
-        $('.dropdown.open').each(function (idx, item) {
-            if ($(item)[0] != self[0]) {
-                $(item).removeClass('open');
-            }
-        })
-    });
+    // jQuery('.dropdown-toggle').click(function () {
+    //     var self = $(this).parent();
+    //     $('.dropdown.open').each(function (idx, item) {
+    //         if ($(item)[0] != self[0]) {
+    //             $(item).removeClass('open');
+    //         }
+    //     })
+    // });
 
-    jQuery17('.carousel').carousel();
+    jQuery('.carousel').carousel();
+
+    // search button should work as search when field not collapsed
+    $('#searchinput').on('hidden.bs.collapse', function () {
+        $(this).next().attr('type', 'button');
+    })
+
+    $('#searchinput').on('hide.bs.collapse', function (e) {
+        $('#nav li + li a').removeClass('narrow');
+        $search = $(this).find('input');
+        console.log($search.val());
+        if ($search.val() != '') {
+            $(this).parent('form').submit();
+        }
+    })
+
+    $('#searchinput').on('show.bs.collapse', function () {
+        $('#nav li a').addClass('narrow');
+    })
+
 })
