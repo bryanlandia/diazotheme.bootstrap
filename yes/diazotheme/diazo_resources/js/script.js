95a0d39c3255b20f899bcefddbcdd8c04755c9dd
Bryan Wilson <bryanlandia@gmail.com>
2014-09-04 08:48:36 -0700

    Separate top nav elements by media query/Response.js Style mobile top navigation. Add separate mobile small logo. Use Open Sans for tagline in both logo versions.

--- yes/diazotheme/diazo_resources/js/script.js
+++ yes/diazotheme/diazo_resources/js/script.js
@@ -1,39 +1,90 @@
 /* Author: 
 
 */
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
 
 $(document).ready(function () {
+    console.log('jquery document ready');
     $('.field.error').each(function (idx, el) {
         if ($.trim($(el).text()) == '') {
             $(el).remove();
         }
     });
-    // jQuery('.dropdown-toggle').click(function () {
-    //     var self = $(this).parent();
-    //     $('.dropdown.open').each(function (idx, item) {
-    //         if ($(item)[0] != self[0]) {
-    //             $(item).removeClass('open');
-    //         }
-    //     })
-    // });
 
     jQuery('.carousel').carousel();
 
-    // search button should work as search when field not collapsed
-    $('#searchinput').on('hidden.bs.collapse', function () {
-        $(this).next().attr('type', 'button');
-    })
+});
 
-    $('#searchinput').on('hide.bs.collapse', function (e) {
-        $('#nav li a').removeClass('narrow');
-        $search = $(this).find('input');
-        if ($search.val() != '') {
-            $(this).parent('form').submit();
+
+
+function bindSearchInput() {
+  console.log('binding search events');
+  $('#searchinput').on('hidden.bs.collapse', function () {
+      console.log('hidden.bs.collapse');
+      $(this).next().attr('type', 'button');
+  })
+
+  $('#searchinput').on('hide.bs.collapse', function (e) {
+      console.log('hide.bs.collapse');
+      $('#nav li a').removeClass('narrow');
+      $search = $(this).find('input');
+      if ($search.val() != '') {
+          $(this).parent('form').submit();
+      }
+  })
+
+  $('#searchinput').on('show.bs.collapse', function () {
+      console.log('show.bs.collapse');
+      $('#nav li a').addClass('narrow');
+  })
+}
+
+
+
+Response.crossover(function(){
+  bindSearchInput();
+}, "width");
+
+
+if (MutationObserver != undefined) {
+
+    var observer = new MutationObserver(function( mutations ) {
+      mutations.forEach(function( mutation ) {
+        var newNodes = mutation.addedNodes;
+        if( newNodes !== null ) {
+          var $nodes = $(newNodes).find('#searchinput');
+          $nodes.each(function() {
+            var $node = $(this);
+            if( $node.is("#searchinput") ) {
+              // we have our search input now
+              // search button should work as search when field not collapsed
+              bindSearchInput();
+              observer.disconnect();
+            }
+          });
         }
-    })
+      });    
+    });
 
-    $('#searchinput').on('show.bs.collapse', function () {
-        $('#nav li a').addClass('narrow');
-    })
+    var config = { 
+      attributes: true, 
+      childList: true, 
+      characterData: true,
+      subtree: true
+    };
+     
+    observer.observe(window.document, config);
 
-})
+} 
+
+else {
+    $(document).on("DOMNodeInserted", function (e) {
+      //console.log(e.target);
+      if (e.target.is('#searchinput')) {
+        bindSearchInput();
+        $(document).off("DOMNodeInserted");
+      }
+    })
+}
+
